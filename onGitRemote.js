da1fc983482ffaf6df6b1d11014364e757d69dc7
Raaghav <raaghav.4243@gmail.com>
2021-12-19 19:35:29 +0530

    pushed using git push

--- onGitRemote.js
+++ onGitRemote.js
@@ -1,2 +1,3 @@
 //  this page is created on github remote repository lets see if we get these changes in local repo
 // now we used git pull to pull the changes which took place on remote repository
+// git push is used to push changes on the local repository, now lets push it using git push
