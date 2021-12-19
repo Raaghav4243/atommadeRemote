f3240bea50568e3e5fe11aad2490fe3bcf71b45d
Raaghav <raaghav.4243@gmail.com>
2021-12-19 19:33:17 +0530

    commited after pull in onGitRemote file

--- onGitRemote.js
+++ onGitRemote.js
@@ -1,1 +1,2 @@
 //  this page is created on github remote repository lets see if we get these changes in local repo
+// now we used git pull to pull the changes which took place on remote repository
